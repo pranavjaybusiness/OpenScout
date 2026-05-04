406b594a163f57ed80e1ea3734041540650906d9
Pranav Jay <pranavjaybusiness@gmail.com>
2026-05-04 04:05:40 -0700

    typo

--- extension/background.js
+++ extension/background.js
@@ -18,7 +18,6 @@ class ApiHandler {
     }
 }
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // If the URL changed, tell the content script to re-evaluate the page
     if (changeInfo.url || changeInfo.status === 'complete') {
         chrome.tabs.sendMessage(tabId, { action: "page_navigated" }).catch(() => {});
     }
@@ -27,7 +26,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "process_text") {
         const api = new ApiHandler("http://127.0.0.1:8000/parse");
         
-        // Return true immediately to tell Chrome we will respond asynchronously
         api.sendToBackend(request.payload).then(data => {
             sendResponse({ result: data });
         });
